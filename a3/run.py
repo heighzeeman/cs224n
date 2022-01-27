b5095e7d18305ab2c19528bed4ecd765fd083835
Matty Wong <wonghzm@stanford.edu>
2022-01-27 00:16:39 -0800

    prelim run.py

--- a3/run.py
+++ a3/run.py
@@ -51,9 +51,10 @@ def train(parser, train_data, dev_data, output_path, batch_size=1024, n_epochs=1
     ###       necessary parameters to tune.
     ### Please see the following docs for support:
     ###     Adam Optimizer: https://pytorch.org/docs/stable/optim.html
-    ###     Cross Entropy Loss: https://pytorch.org/docs/stable/nn.html#crossentropyloss
-
-
+    ###     Cross Entropy Loss:     
+    
+    optimizer = optim.Adam(parser.model.parameters())
+    loss_func = nn.CrossEntropyLoss()
 
     ### END YOUR CODE
 
@@ -106,8 +107,10 @@ def train_for_epoch(parser, train_data, dev_data, optimizer, loss_func, batch_si
             ### Please see the following docs for support:
             ###     Optimizer Step: https://pytorch.org/docs/stable/optim.html#optimizer-step
 
-
-
+            logits = parser.model.forward(train_x)
+            loss = loss_func(train_y, logits)
+            loss.backward()
+            optimizer.step()
 
             ### END YOUR CODE
             prog.update(1)
