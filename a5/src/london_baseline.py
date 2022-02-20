d624e80838e20d66ea0729ec616ed5573917994d
Matty Wong <wonghzm@stanford.edu>
2022-02-20 04:51:18 -0800

    London baseline

--- a5/src/london_baseline.py
+++ a5/src/london_baseline.py
@@ -2,3 +2,14 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+filepath = "../birth_dev.tsv"
+with open(filepath) as fin:
+    lines = [x.strip().split('\t') for x in fin]
+    if len(lines[0]) == 1:
+        print('No gold birth places provided; returning (0,0)')
+    true_places = [x[1] for x in lines]
+    total = len(true_places)
+    predicted_places = [ "London" for _ in range(total) ]
+    correct = len(list(filter(lambda x: x[0] == x[1],
+      zip(true_places, predicted_places))))
+    print("London baseline: (tot, correct)", (float(total),float(correct)))
